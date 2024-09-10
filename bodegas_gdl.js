3832aa6c5b2ba9b8210beb1a3d4821af71f2aaa8
librekcesar <140665209+librekcesar@users.noreply.github.com>
2024-09-09 18:46:15 -0600

    Add files via upload

---
+++ bodegas_gdl.js
@@ -0,0 +1,10 @@
+var bodegas = {
+"type": "FeatureCollection",
+"name": "bodegas_gdl",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "distancia": 2286.97, "nombre": "\"0 de noviembre", "direccion": "Calle 20 de Noviembre 623, Barragán Hernández, 44469 Guadalajara, Jal.", "dist_km": 2.287 }, "geometry": { "type": "Point", "coordinates": [ -103.344729714821796, 20.666338702722154 ] } },
+{ "type": "Feature", "properties": { "id": 2, "distancia": 6826.129, "nombre": "Ildefonso", "direccion": "San Gabriel 1606, Real del Parque, 45140 Zapopan, Jal.", "dist_km": 6.826 }, "geometry": { "type": "Point", "coordinates": [ -103.406655466466347, 20.723262006761971 ] } },
+{ "type": "Feature", "properties": { "id": 3, "distancia": 2945.495, "nombre": "Independencia", "direccion": "Calz Independencia sur 1825. Rincón de agua azul Guadalajara, Jal.", "dist_km": 2.945 }, "geometry": { "type": "Point", "coordinates": [ -103.353654708181594, 20.651820368380076 ] } }
+]
+}
---
+++ centroide_volumen.js
@@ -0,0 +1,8 @@
+var centroide_volumen = {
+"type": "FeatureCollection",
+"name": "centroide_volumen",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "country": "MEX", "locality": "San Pedro Tlaquepaque", "state": "Jalisco", "zonificacion": "TLAQ_6", "catstationid": "7", "station_code": "SPGDL3", "zipcode": "45560", "region": "SP_Pacífico", "station_name": "Zapopan", "id": "3072", "coverage_type": "Local", "cobertura_diferenciada": "No Diferenciado", "cluster_diferenciado": "ZM", "polygon": "1", "neighborhood": "Centroi de volumen", "fid": "2905", "promedio": 29, "asignados": 746, "entregados": 638, "densidad": 102.939, "area_m2": 7246767.456, "km2": 7.247 }, "geometry": { "type": "Point", "coordinates": [ -103.363731564813222, 20.676677325137501 ] } }
+]
+}
